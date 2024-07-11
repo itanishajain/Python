3a40afcb13835549dcab6c35b7c089debabc7a12
Tanisha Jain <itanishajain@gmail.com>
2024-07-11 11:55:13 +0530

    Starting of python

---
+++ first.py
@@ -0,0 +1,20 @@
+print("Hello Peeps!")
+
+#EXERCISE - 1
+
+First_name = "Tony"
+Last_name = "Stark"
+age = 51
+is_genius = True
+print(First_name, Last_name, "age is", age, "Is he a genius ?", is_genius)
+
+#EXERCISE - 2
+
+superhero = input("Tony what is your superhero name? ")
+print(superhero)
+
+#Sum of 2 number
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+sum = num1 + num2
+print("The sum is:" , sum)
