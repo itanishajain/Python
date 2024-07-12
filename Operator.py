368d6f8fb08d9105de611bb1ae924641d84dde2f
Tanisha Jain <itanishajain@gmail.com>
2024-07-12 12:41:18 +0530

    Airthmetic Operator

---
+++ Operator.py
@@ -0,0 +1,9 @@
+# Arithmetic Operator
+
+print(8+2)   # Addition
+print(3-1)   # Substraction
+print(9/2)   # Division
+print(5*6)   # Multiplication
+print(9//2)  # Floor Division -> It will return only integer value after division
+print(3**4)  # Exponention
+print(7%5)   # Modulo -> It gives remainder after division
