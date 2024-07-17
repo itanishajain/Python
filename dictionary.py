63a3f4a42b2a4cce573581ddf7db07422c1223f9
Tanisha Jain <itanishajain@gmail.com>
2024-07-17 18:54:14 +0530

    Dictionary

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+marks = {"Maths" : 95, "Computer" : 92, "English" : 84}
+print(marks["Maths"])
+
+# To add new key:value pair
+marks["Physics"] = 77
+print(marks)
+
+# We can chnage the content of value of a specific key
+marks["Physics"] = 80
+print(marks)
